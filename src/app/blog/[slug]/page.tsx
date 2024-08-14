656359aa3ab17e464d5ca37a507c776fa531a870
umarluqman <umarluqman.78@gmail.com>
2024-08-14 13:17:23 +0800

    init data for pusat dialisis

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import { format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
-
+import dayjs from "dayjs";
+import Link from "next/link";
 // This generates all teh blog posts paths
 export async function generateStaticParams() {
   const postSlugs = allPosts.map((post) => post.slug);
@@ -40,7 +39,7 @@ const PostLayout = async ({ params }: { params: { slug: string } }) => {
         <div className="mb-6 text-center">
           <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
           <time dateTime={post.date} className="text-sm text-slate-600">
-            {format(parseISO(post.date), "LLLL d, yyyy")}
+            {dayjs(post.date).format("MMMM D, YYYY")}
           </time>
         </div>
         <div
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
-import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
-
+import dayjs from "dayjs";
+import Link from "next/link";
 async function getAllPosts() {
   const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date));
+    return dayjs(b.date).diff(dayjs(a.date));
   });
   return posts;
 }
@@ -13,7 +12,7 @@ function PostCard(post: { date: string; url: string; title: string }) {
   return (
     <div className="mb-6">
       <time dateTime={post.date} className="block text-sm text-slate-600">
-        {format(parseISO(post.date), "LLLL d, yyyy")}
+        {dayjs(post.date).format("MMMM D, YYYY")}
       </time>
       <h2 className="text-lg">
         <Link href={post.url}>{post.title}</Link>
