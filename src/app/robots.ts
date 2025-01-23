54434868cb8f21e3421160d170cd705ae9023a31
umarluqman <umarluqman.78@gmail.com>
2025-01-23 19:05:55 +0800

    let robot index dialisis my assets

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/*", "/_next/*", "/404", "/500"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/", "/favicon.ico", "/site.webmanifest"],
+        disallow: ["/api/*", "/_next/*", "/404", "/500"],
+      },
+    ],
     sitemap: "https://dialisis.my/sitemap.xml",
   };
 }
