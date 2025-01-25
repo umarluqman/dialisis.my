2dc98f2f6671e6a46923380cee93d39dde50e11d
umarluqman <umarluqman.78@gmail.com>
2025-01-25 21:56:50 +0800

    fixed og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     siteName: "Dialisis MY",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://dialisis.my/og-image.png",
         width: 1200,
         height: 630,
         alt: "Dialisis MY - Cari Pusat Dialisis Malaysia",
