e036d4b42134f8e7398c2dec5483545b1abb2de4
umarluqman <umarluqman.78@gmail.com>
2026-05-03 18:10:16 +0800

    deploy the fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,12 @@
 import path from 'node:path'
-import { defineConfig } from 'prisma/config'
+import { defineConfig, env } from 'prisma/config'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: path.join(__dirname, '.env.local') })
 
 export default defineConfig({
   schema: path.join(__dirname, 'prisma/schema.prisma'),
+  datasource: {
+    url: 'file:./prisma/dev.db',
+  },
 })
